1cbcae5d841eebb8219e4a0c167a1199c71ef77b
Iris Díaz Feijóo <irisdf_4@hotmail.com>
2024-10-09 16:05:43 +0200

    Terminado ejercicio MinMaxValue y  Cast

---
+++ src/main/java/types/Cast.java
@@ -0,0 +1,23 @@
+package types;
+
+public class Cast {
+    public static void main(String[] args) {
+        int originalInt = 200;
+        originalInt = originalInt + 2;
+        System.out.println(originalInt);
+        String originalString =  "My number is: ";
+        System.out.println(originalString + 2);
+        System.out.println(originalString);
+        String parsedIn = String.valueOf(originalInt);
+        parsedIn = parsedIn + 2;
+        System.out.println(parsedIn);
+        boolean bool = false;
+        String strBool = String.valueOf(bool);
+        System.out.println(bool);
+        System.out.println(strBool);
+
+
+
+
+    }
+}
---
+++ src/main/java/types/MinMaxValues.java
@@ -0,0 +1,25 @@
+package types;
+
+public class MinMaxValues {
+    public static void main(String[] args) {
+
+        System.out.println("Minimum byte value: " + Byte.MIN_VALUE);
+        System.out.println( "Maximun byte valura: " + Byte.MAX_VALUE);
+
+        System.out.println("Minimum short value: " + Short.MIN_VALUE);
+        System.out.println( "Maximun short valura: " + Short.MAX_VALUE);
+
+        System.out.println("Minimum integer value: " + Integer.MIN_VALUE);
+        System.out.println( "Maximun integer valura: " + Integer.MAX_VALUE);
+
+        System.out.println("Minimum long value: " + Long.MIN_VALUE);
+        System.out.println( "Maximun long valura: " + Long.MAX_VALUE);
+
+        System.out.println("Minimum float value: " + Float.MIN_VALUE);
+        System.out.println( "Maximun byte valura: " + Float.MAX_VALUE);
+
+        System.out.println("Minimum double value: " + Double.MIN_VALUE);
+        System.out.println( "Maximun double valura: " + Double.MAX_VALUE);
+
+    }
+}
