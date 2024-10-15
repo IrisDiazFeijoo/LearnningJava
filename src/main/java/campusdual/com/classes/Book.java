23f9c2325c5c00a09432aff96f552a2c6843cd55
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-15 08:37:27 +0200

    Wrip classes

---
+++ src/main/java/campusdual/com/classes/Book.java
@@ -0,0 +1,14 @@
+package campusdual.com.classes;
+
+public class Book {
+    public String title;
+    public String author;
+    public String pageNumber;
+
+    public Book(String title, String author, int numberOfPages){
+        this.title = title;
+        this.author = author;
+        this.pageNumber = numberOfPages;
+
+    }
+}
